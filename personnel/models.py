10fd07c1273c29d9bd357ab063469a16f13d543f
Laeeq <laeeqkhan01@gmail.com>
2020-10-04 12:25:21 +0530

    Added model/table for Worker in personnel app

--- personnel/models.py
+++ personnel/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.conf import settings
+from django.db   import models
 
 # Create your models here.
+
+class Worker(models.Model):
+    name   = models.CharField(max_length=32)
+    age    = models.IntegerField()
+    dept   = models.CharField(max_length=16)
+    salary = models.IntegerField()
+
+    def gets_raise(self):
+      self.salary *= 1.1
+
+    def __str__(self):
+      return self.name
+    

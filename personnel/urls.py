4f46f3c41e50c7fd769428ebd4d2189cbd43c898
Laeeq <laeeqkhan01@gmail.com>
2020-10-05 02:27:26 +0530

    Added a page to display full employee details

--- personnel/urls.py
+++ personnel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
-  path('', views.homePage, name='homePage'),
+  path(''                , views.homePage     , name='homePage'),
+  path('worker/<int:pk>/', views.worker_detail, name='worker_detail'),
 ]
--- personnel/views.py
+++ personnel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http      import HttpResponse
 from .models          import Worker
 
@@ -7,3 +7,7 @@ from .models          import Worker
 def homePage(request):
     allWorkers = Worker.objects.all()
     return render(request, 'personnel/homePage.html', {'allWorkers': allWorkers})
+
+def worker_detail(request, pk):
+    wrkrDetail = get_object_or_404(Worker, pk=pk)
+    return render(request, 'personnel/worker_detail.html', {'wrkrDetail': wrkrDetail})

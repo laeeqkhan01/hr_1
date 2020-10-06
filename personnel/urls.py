7de05ec91522049df616f968c75303af00ee3bdf
Laeeq <laeeqkhan01@gmail.com>
2020-10-06 10:09:18 +0530

    Added capability to create new employee entries

--- personnel/urls.py
+++ personnel/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
-  path(''                , views.homePage     , name='homePage'),
+  path(''                , views.homePage     , name='homePage'     ),
   path('worker/<int:pk>/', views.worker_detail, name='worker_detail'),
+  path('new_worker/'     , views.new_worker   , name='new_worker'   ),
 ]
--- personnel/views.py
+++ personnel/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http      import HttpResponse
 from .models          import Worker
+from .forms           import WorkerForm
 
 # Create your views here.
 
@@ -11,3 +12,21 @@ def homePage(request):
 def worker_detail(request, pk):
     wrkrDetail = get_object_or_404(Worker, pk=pk)
     return render(request, 'personnel/worker_detail.html', {'wrkrDetail': wrkrDetail})
+
+def new_worker(request):
+  # We come here twice, once at the time of rendering/displaying a blank form
+  # and again when the submit button on the form is clicked.
+  if request.method == "POST":  # Data received from the filled form
+    recvdForm = WorkerForm(request.POST) #Create a form with POST data
+    if recvdForm.is_valid():
+      # Get the row entry without saving it in the table, so it can be edited.
+      newEntry = recvdForm.save(commit=False)
+      if newEntry.age > 100:
+        newEntry.age = 100  # A demo editing.
+      newEntry.save() # Now save in the table
+      return redirect('worker_detail', pk=newEntry.pk) # This should match urls.py entry
+    else:
+      return HttpResponse("-----InValid Form data received -------")
+  else: # Display blank form, so user can fill the new worker data
+    wrkrForm = WorkerForm() # Create an empty form
+    return render(request, 'personnel/wrkrDataForm.html', {'wrkrForm': wrkrForm})

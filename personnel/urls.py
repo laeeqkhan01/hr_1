db59785fd8a3dafb5a44ed0303eadb3c979238df
Laeeq <laeeqkhan01@gmail.com>
2020-10-06 20:12:28 +0530

    Added capability to modify employee info

--- personnel/urls.py
+++ personnel/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
-  path(''                , views.homePage     , name='homePage'     ),
-  path('worker/<int:pk>/', views.worker_detail, name='worker_detail'),
-  path('new_worker/'     , views.new_worker   , name='new_worker'   ),
+  path(''                     , views.homePage     , name='homePage'        ),
+  path('worker/<int:pk>/'     , views.worker_detail, name='worker_detail'   ),
+  path('worker/<int:pk>/edit/', views.worker_edit  , name='worker_edit_info'),
+  path('new_worker/'          , views.new_worker   , name='new_worker'      ),
 ]
--- personnel/views.py
+++ personnel/views.py
@@ -30,3 +30,27 @@ def new_worker(request):
   else: # Display blank form, so user can fill the new worker data
     wrkrForm = WorkerForm() # Create an empty form
     return render(request, 'personnel/wrkrDataForm.html', {'wrkrForm': wrkrForm})
+
+def worker_edit(request, pk):
+  # We come here twice, once at the time of rendering/displaying a form filled
+  # with data from table[pk], and again after submit button is clicke.
+  if request.method == "POST":
+    # Get the row from table corresp to this pk
+    row = get_object_or_404(Worker, pk=pk)
+    # Create a form from the POST values, corresp to above row
+    form = WorkerForm(request.POST, instance=row)
+    if form.is_valid():
+      # Get editable version of the row object
+      editable_row_obj = form.save(commit=False)
+      if editable_row_obj.age > 100:
+        editable_row_obj.age = 100  # Demo editing
+      editable_row_obj.save() # Save the row in the table
+      return HttpResponse("<h1>Data Saved in DB</h1>")
+    else:
+      return HttpResponse("<h1>Invalid Data received from the form</h1>")
+  else:
+    # Get the object from the table corresp to this pk.
+    row = get_object_or_404(Worker, pk=pk)
+    # Create a form with values from the selected row
+    wrkrForm = WorkerForm(instance = row)
+    return render(request, 'personnel/wrkrDataForm.html', {'wrkrForm' : wrkrForm})
